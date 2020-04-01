23351a9dd90bd1969823d6a94ade8bf0df60af8c
schernov <s.chernov@just-ai.com>
2020-04-01 14:34:15 +0300

    fix quotes

--- src/main/java/com/justai/cm/core/actions/Push.java
+++ src/main/java/com/justai/cm/core/actions/Push.java
@@ -42,7 +42,7 @@ public class Push extends BaseAction {
 
         // set permissions
         sshManager.doWithConnection(cmp.getZHost(), ssh -> {
-            ssh.exec(new String[]{"sh", "-c", "chmod +x `find " + remoteFolder + " -name '*.sh'`"});
+            ssh.exec(new String[]{"sh", "-c", "chmod +x `find " + remoteFolder + " -name \"*.sh\"`"});
         });
 
         // execute config map
